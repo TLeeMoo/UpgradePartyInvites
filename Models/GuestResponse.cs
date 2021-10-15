a246fb8480273c0b72854b28565c9f5c4fbce8f8
TLeeMoo <bithiahmcgriff@gmail.com>
2021-10-15 14:25:54 -0700

    Add project files.

---
+++ Models/GuestResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace UpgradePartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "please enter your phone number") ]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
